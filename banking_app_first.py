dcf552478c1072b7e75ff6f644fdc6432ba24ab9
vaida valatkaite <vaid.valat@gmail.com>
2025-03-12 18:37:28 +0200

    updating files

---
+++ banking_app_first.py
@@ -0,0 +1,13 @@
+# terminal based
+
+# Ask user to create account 
+# (In that case you need to provide your email address, 
+# and then the app will give you ID and password)
+
+
+# Log In to bank service (you check email and password . 
+# Email and ID should be linked) email -> ID -> password.
+
+# separate modules, class structures and access modifiers (private and protected)
+
+
