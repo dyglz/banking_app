a615e106c8190c6d2006f19e68721c806294c343
vaida valatkaite <vaid.valat@gmail.com>
2025-03-17 14:02:25 +0200

    updating files

--- banking_app.py
+++ banking_app.py
@@ -10,4 +10,30 @@
 
 # separate modules, class structures and access modifiers (private and protected)
 
+while True:
+    print("===  Welcome to BestBank App!  ===")
+    print("1. Create Account\n2. Log In\n0. Exit App")
+    
+    try:
+        menu_selection = int(input("Enter your selection: "))
+        if 0 <= menu_selection <=2:
+            if menu_selection == 0:
+                print("===    Exiting BestBank App    ===")
+                break
+            elif menu_selection == 1:
+                pass
+            elif menu_selection == 2:
+                pass   
+        else:
+            print("Invalid selection!")
+    except ValueError:
+        print("Invalid symbol entered!")
+    except Exception as e:
+        print(f"Unexpected error occured: {e}")
+
+
+
+
+
+
 
